71afa44eeb7f7c39d1f00d36cd90b6fdb5cb137b
leandrops03 <uvaleandro512@gmail.com>
2025-09-22 21:35:25 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,9 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./jogos.db"
+# URL de conexão com o banco de dados (aqui estamos usando SQLite para simplicidade)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
+# Criação da engine para conectar ao banco de dados
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+# Criando uma sessão que será usada nas operações com o banco
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base que será usada para criar os modelos (tabelas)
 Base = declarative_base()
+
+# Função para obter a sessão do banco
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
